1b7a71075d66524b80adc170b83609fc95fb76bb
carmalou <carmenbourlonlong@gmail.com>
2017-03-14 13:07:11 -0500

    swap out readme

---
+++ src/Components/movie.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+class Movie extends React.Component {
+  render() {
+    return <h1>Hello World</h1>;
+  }
+}
