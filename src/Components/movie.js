ab87be120fb4f54bb97356270b9ef019ab43a449
Carmen Long <carmen.long@hobbylobby.com>
2018-06-19 09:07:14 -0500

    update for wonder woman 1984

--- src/Components/movie.js
+++ src/Components/movie.js
@@ -42,7 +42,7 @@ export default class Movie extends React.Component {
       <div className="parent-div center-align">
         <div className="center-align">
           <h1 className="App">Can I go see {movieName} yet?</h1>
-          <h1 className="App">{yesOrNo}. <a href="http://www.fandango.com/wonderwoman_191725/movietimes" target="_blank">Get your ticket now.</a></h1>
+          <h1 className="App">{yesOrNo}. <a href="https://www.fandango.com/wonder-woman-1984-212474/movie-overview" target="_blank">Get your ticket now.</a></h1>
           <h1 className="App">You can go see Wonder Woman {this.state.howMuchLonger}</h1>
         </div>
       </div>
